430743051c55b940a12cc64403491a60ed53a4af
sandeep kadu <sandeepkadu41@gmail.com>
2025-01-22 20:57:56 +0530

    Initial commit of Spring Boot JDBC Project

---
+++ src/main/java/com/sandeep/SpringBootJdbcController.java
@@ -0,0 +1,17 @@
+package com.sandeep;
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.jdbc.core.JdbcTemplate;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+public class SpringBootJdbcController {  
+    @Autowired  
+    JdbcTemplate jdbc;    
+    @RequestMapping("/insert")  
+    public String index(){  
+        //for mysql  //jdbc.execute("insert into user(name,email)values('sandeepKadu','sk@yopmail.com')");  
+    	jdbc.execute("INSERT INTO user (name, email) VALUES ('sk', 'sbk1.kadu@yopmail.com')");
+        return "data inserted Successfully";  
+    }  
+}  
